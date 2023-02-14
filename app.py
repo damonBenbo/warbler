10cc2d900fc1aae40dc34362b472b665183200df
damon <damon@DESKTOP-7TRRA9Q.localdomain>
2023-02-13 21:16:35 -0700

    added logout with redirect

--- app.py
+++ app.py
@@ -18,9 +18,10 @@ app.config['SQLALCHEMY_DATABASE_URI'] = (
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = False
-app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = True
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "it's a secret")
 toolbar = DebugToolbarExtension(app)
+
 
 connect_db(app)
 
@@ -114,6 +115,10 @@ def logout():
     """Handle logout of user."""
 
     # IMPLEMENT THIS
+
+    do_logout()
+
+    return redirect('/')
 
 
 ##############################################################################
